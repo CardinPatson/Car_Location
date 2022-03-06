ce6cfe5fc546540ffe9e5dd94dcbc917529c33a4
unknown <mi;pourbaix#students.ephec.be>
2022-03-06 09:50:19 +0100

    Adding a path in App.js to the connexion/registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Cars from "./components/cars";
 import AddCars from "./components/admin/addCars";
+import Connreg from "./components/connreg";
+
 function App() {
 	return (
 		<Router>
@@ -10,6 +12,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/cars" element={<Cars name="mercedes" />} />
 				<Route path="/add-cars" element={<AddCars />} />
+				<Route path="/connreg" element={<Connreg />}/>
 			</Routes>
 		</Router>
 	);
