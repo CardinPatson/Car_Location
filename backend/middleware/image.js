a94e7b7b28d49206a46f5e80e9e6a1ca2dcd6c0a
CardinPatson <cardin.tiako@gmail.com>
2022-03-16 19:16:09 +0100

    [ADD] Complete middleware image

--- backend/middleware/image.js
+++ backend/middleware/image.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
-
+const { promisify } = require("util");
 const MIME_TYPES = {
-	"images/jpeg": "jpg",
+	"image/jpeg": "jpg",
 	"image/jpg": "jpg",
 	"image/png": "png",
 };
@@ -11,15 +11,15 @@ const storage = multer.diskStorage({
 		//Config pour la destination des images
 		callback(null, "images");
 	},
-	filename: (req, files, callback) => {
+	filename: (req, file, callback) => {
 		//Config pour rendre le nom de l'image unique
-		for (let file in files) {
-			console.log(file);
-			const name = file.originalname.split(" ").join("_");
-			const extension = MIME_TYPES[file.mimetype];
-			callback(null, `${name}${Date.now()}.${extension}`);
-		}
+		const name = file.originalname.split(" ").join("_");
+		const extension = MIME_TYPES[file.mimetype];
+		callback(null, `${name}${Date.now()}.${extension}`);
 	},
 });
-
-module.exports = multer({ storage: storage });
+//image est le nom de la clé initialisé dans le formData  10 le nombre de fichier max
+const multerFiles = multer({ storage: storage }).array("image", 10);
+//pour que le middleware soit utilisé avec async et await
+const multerFilesMiddleware = promisify(multerFiles);
+module.exports = multerFilesMiddleware;
