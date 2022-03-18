7cdc72bb1b438c8e9eadafd4c1afce463be329e7
CardinPatson <cardin.tiako@gmail.com>
2022-03-18 10:50:36 +0100

    [ADD] reducer for dispatch info

--- frontend/src/reducer/carReducer.js
+++ frontend/src/reducer/carReducer.js
@@ -2,38 +2,40 @@
 import { addCarsInfo } from "../action/carAction";
 import { createReducer } from "@reduxjs/toolkit";
 
-const initialState = {
-  name: "",
-  description: "",
-  brand: "",
-  model: "",
-  color: "",
-  doors: 0,
-  bootSize: 0,
-  energy: "",
-  passengers: 0,
-  type: "", //sport ou suv
-  price: 0,
-  airCondition: false,
-  isAutomatic: false,
-  images: [],
-};
+//FAIRE DU STATE INITIALE UN TABLEAU VIDE
+
+// const initialState = {
+//   name: "",
+//   description: "",
+//   brand: "",
+//   model: "",
+//   color: "",
+//   doors: 0,
+//   bootSize: 0,
+//   energy: "",
+//   passengers: 0,
+//   type: "", //sport ou suv
+//   price: 0,
+//   airCondition: false,
+//   isAutomatic: false,
+//   images: [],
+// };
+const initialState = { cars: [] };
 
 //L'action addCars ne renvoie rien mais déclenchera une rafraichissement pour prendre en compte le nouveau véhicule rajouté
 
 //CE REDUCER SERA UTILISE DANS LE CAS DUNE RECUPERATION DE VOITURE POUR LA SAUVEGARDE DE LETAT DES VOITURES EN LOCALE
-export const carReducer = createReducer(initialState, (builder, state) => {
-  builder.addCase(addCarsInfo, (state, action) => {
-    state.name = action.payload.name;
-    state.description = action.payload.description;
-    state.brand = action.payload.brand;
-    state.model = action.payload.model;
-    state.price = action.payload.price;
-    state.doors = action.payload.doors;
-    state.bootSize = action.payload.bootSize;
-    state.type = action.payload.type;
-    state.energy = action.paylad.energy;
-  });
+export const carReducer = createReducer(initialState, (builder) => {
+	builder
+		.addCase(addCarsInfo, (state, action) => {
+			// state.push(action.payload);
+			// state = [...state.slice(0, state.length)];
+			state.cars = action.payload;
+			
+		})
+		.addDefaultCase((state, action) => {
+			return state;
+		});
 });
 
 //Reducer pour la recupération des voitures
