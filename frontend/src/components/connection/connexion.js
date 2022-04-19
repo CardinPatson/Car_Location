23585895359a2de440916b1ddc22173d6a2fd92e
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-04-19 10:09:15 +0200

    [test]

--- frontend/src/components/connection/connexion.js
+++ frontend/src/components/connection/connexion.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Checkbox } from '@mui/material';
 
 function Connexion(props) {
 	const handleConnexion = () => {
@@ -20,6 +21,11 @@ function Connexion(props) {
 						<input type="password" />
 					</Password>
 					<Confirm>
+						<Remember>
+							<Checkbox>
+							</Checkbox>
+							Se souvenir de moi
+						</Remember>
 						<button
 							onClick={(e) => {
 								handleConnexion(e);
@@ -27,11 +33,6 @@ function Connexion(props) {
 						>
 							Se connecter
 						</button>
-						<Remember>
-							Se souvenir de moi
-							<input type="checkbox" />
-							<span></span>
-						</Remember>
 					</Confirm>
 					<Options>
 						<Forgotpassword>
@@ -61,10 +62,9 @@ const Content = styled.div`
 	margin-top: 30%;
 	display: flex;
 	flex-direction: column;
-	border: solid 0.5px rgba(0, 0, 0, 0.08);
 	border-radius: 3px;
 	background-color: rgb(255, 255, 255, 0.9);
-	box-shadow: 0 0 1px black;
+	box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 `;
 
 const Banner = styled.div`
@@ -80,6 +80,8 @@ const Banner = styled.div`
 const Form = styled.form`
     display: flex;
     flex-direction: column;
+	gap: 15px;
+	margin: 10px;
     p{
         text-align: left;
         font-size: 3vh;
@@ -87,17 +89,17 @@ const Form = styled.form`
     input {
         border: 1px solid #797979;
         border-radius:0.5vh;
-        height: 4vh;
-        font-size: 2.5vh;
-        width: 40vh;
+        height: 4.5vh;
+        font-size: 3vh;
+        width: 90%;
     }
     button {
-        font-size: 3vh;
+        font-size: 3.5vh;
         color: #333333;
+		border: 0;
         background-color: #00A9FF;
-        border: 1.5px solid #00486D;
         border-radius: 1vh;
-        width 40%;
+        width: 50%;
         padding: 1vh;
         margin: 2vh 1vh 2vh 1vh;
         cursor: pointer;
@@ -105,7 +107,7 @@ const Form = styled.form`
     button:hover{
         color: white;
         background-color: #0078B5;
-        border: 1.5px solid #00A9FF;
+		border: 0;
     }
     button:active {
         transform: scale(0.95);
@@ -115,6 +117,7 @@ const Form = styled.form`
 const Login = styled.div`
 	margin: 1vh;
 	display: flex;
+	gap: 5px;
 	justify-content: flex-start;
 	flex-direction: column;
 `;
@@ -122,88 +125,47 @@ const Login = styled.div`
 const Password = styled.div`
 	margin: 1vh;
 	display: flex;
+	gap: 5px;
 	justify-content: flex-start;
 	flex-direction: column;
 `;
 
 const Confirm = styled.div`
 	display: flex;
-	flex-direction: row;
+	flex-direction: column;
+	gap: 15px;
+	button {
+		align-self: center;
+	}
 `;
 
 const Remember = styled.label`
-	display: block;
-	position: relative;
-	padding-left: 35px;
-	cursor: pointer;
-	font-size: 2.5vh;
-	-webkit-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
-	top: 35%;
-	left: 5%;
-	height: 5vh;
-	input {
-		position: absolute;
-		opacity: 0;
-		cursor: pointer;
-		height: 0;
-		width: 0;
-	}
-	span {
-		position: absolute;
-		top: -3px;
-		left: 0px;
-		height: 25px;
-		width: 25px;
-		background-color: #eee;
-		border: 1px solid grey;
-		border-radius: 15px;
-	}
-	&:hover input ~ span {
-		background-color: #ccc;
-	}
-	& input:checked ~ span {
-		background-color: #2196f3;
-	}
-	span:after {
-		content: "";
-		position: absolute;
-		display: none;
-	}
-	& input:checked ~ span:after {
-		display: block;
-	}
-	& span:after {
-		left: 9px;
-		top: 5px;
-		width: 5px;
-		height: 10px;
-		border: solid white;
-		border-width: 0 3px 3px 0;
-		-webkit-transform: rotate(45deg);
-		-ms-transform: rotate(45deg);
-		transform: rotate(45deg);
-	}
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	font-size: 1vw;
 `;
 
 const Options = styled.label`
 	display: flex;
 	flex-direction: row;
+	align-items: center;
 	gap: 7vh;
 `;
 
 const Forgotpassword = styled.div`
 	display: flex;
-	margin: 1vh 1vh 2vh 2vh;
+	align-items: center;
+	margin: 1vh 1vh 1vh 1vh;
+	cursor: pointer;
 	text-decoration: none;
 	font-size: 2.25vh;
 `;
 
 const NoAccount = styled.label`
 	display: flex;
-	margin: 1vh 1vh 2vh 2vh;
+	align-items: center;
+	margin: 1vh 1vh 1vh 2vh;
 	cursor: pointer;
 	text-decoration: underline #00a9ff;
 	font-size: 2.25vh;
