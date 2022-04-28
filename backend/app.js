0f09e5309f33ce2aa495097afddb5c21f59d9328
CardinPatson <cardin.tiako@gmail.com>
2022-04-28 12:08:57 +0200

    remove google authentification on backend

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const path = require("path");
 const carRoute = require("./routes/car");
 const ordersRoute = require("./routes/orders");
 const usersRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
 
 //Configuration pour les variables d'environnement
 
@@ -45,12 +44,9 @@ app.use(
 		saveUninitialized: false,
 	})
 );
-app.use(passport.initialize());
-app.use(passport.session());
 
 //RESTFULL API
 app.use("/api/cars", carRoute);
 app.use("/api/orders", ordersRoute);
 app.use("/api/users", usersRoute);
-app.use("/", authRoute);
 module.exports = app;
