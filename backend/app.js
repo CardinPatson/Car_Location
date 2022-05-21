c2f937bb45ca0d57453c0f2f2c88853b3fbd2271
Patson <cardin.tiako@gmail.com>
2022-05-21 14:33:50 +0200

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,58 +1,7 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const path = require("path");
-const carRoute = require("./routes/car");
-const ordersRoute = require("./routes/orders");
-const usersRoute = require("./routes/users");
-const adminsRoute = require("./routes/admin");
-const app = express();
+const app = require("./index");
 
-const serverExpress = async (app) => {
-	//middleware
-	app.use(cors());
-	app.use(
-		cors({
-			origin: `${process.env.APP_IP}:${process.env.APP_PORT}`,
-			credentials: true,
-		})
-	);
-	app.use((req, res, next) => {
-		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader(
-			"Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Authorization"
-		);
-		res.setHeader(
-			"Access-Control-Allow-Method",
-			"POST, GET, PUT, DELETE, OPTIONS, PATCH"
-		);
-		next();
-	});
-	app.use(express.json());
-	app.use(cookieParser());
-
-	//middleware pour le stockage des images
-	app.use("/images", express.static(path.join(__dirname, "images")));
-
-	app.use(
-		require("express-session")({
-			secret: "keyboard cat",
-			resave: false,
-			saveUninitialized: false,
-		})
-	);
-
-	//RESTFULL API
-	app.use("/api/cars", carRoute);
-	app.use("/api/orders", ordersRoute);
-	app.use("/api/users", usersRoute);
-	app.use("/api/admins", adminsRoute);
-};
-async () => {
-	await serverExpress(app);
-};
 app.listen(process.env.APP_PORT, async () => {
 	console.log("server running on port " + process.env.APP_PORT);
 });
+
 module.exports = app;
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,49 +5,49 @@ const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
 
 const getAllUsers = async (req, res) => {
-    return;
+	return;
 };
 
 const getUserById = async (req, res) => {
-    return;
+	return;
 };
 
 const getUser = async (req, res) => {
-    try {
-        const { email, password } = req.query;
-        let status = "";
-        const adminData = await admins.findOne({ where: { email: email } });
-        if (adminData) {
-            status = "admin";
-        } else {
-            status = "client";
-        }
-        const data = await users.findOne({ where: { mail: email } });
-        //CHECK IF USERS IN DATABASE
-        if (!data) {
-            res.status(404).json({ error: "Utilisateur introuvable" });
-            return;
-        }
-        //CHECK IF PASSWORD IS THE SAME
-        const hash = await bcrypt.compare(password, data.password);
-        if (!hash) {
-            res.status(401).json({ error: "Mot de passe incorrect" });
-            return;
-        }
-        res.status(200).json({
-            user: data.dataValues,
-            status: status,
-            token: jwt.sign(
-                { user: data.dataValues.mail },
-                "SHORT_HASH_PHRASE",
-                {
-                    expiresIn: "24h"
-                }
-            )
-        });
-    } catch (error) {
-        res.status(500).json({ error });
-    }
+	try {
+		const { email, password } = req.query;
+		let status = "";
+		const adminData = await admins.findOne({ where: { email: email } });
+		if (adminData) {
+			status = "admin";
+		} else {
+			status = "client";
+		}
+		const data = await users.findOne({ where: { mail: email } });
+		//CHECK IF USERS IN DATABASE
+		if (!data) {
+			res.status(404).json({ error: "Utilisateur introuvable" });
+			return;
+		}
+		//CHECK IF PASSWORD IS THE SAME
+		const hash = await bcrypt.compare(password, data.password);
+		if (!hash) {
+			res.status(401).json({ error: "Mot de passe incorrect" });
+			return;
+		}
+		res.status(200).json({
+			user: data.dataValues,
+			status: status,
+			token: jwt.sign(
+				{ user: data.dataValues.mail },
+				"SHORT_HASH_PHRASE",
+				{
+					expiresIn: "24h",
+				}
+			),
+		});
+	} catch (error) {
+		res.status(500).json({ error });
+	}
 };
 
 /**
@@ -59,50 +59,50 @@ const getUser = async (req, res) => {
  * @returns {Object} JSON object
  */
 const addUser = async (req, res) => {
-    try {
-        const { firstName, lastName, email, password } = req.body;
+	try {
+		const { firstName, lastName, email, password } = req.body;
 
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({
-                message: "Invalid data",
-                errors: errors.array()
-            });
-        }
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(422).json({
+				message: "Invalid data",
+				errors: errors.array(),
+			});
+		}
 
-        //CHECK IF EMAIL IS UNIQUE
-        const data = await users.findOne({
-            where: { mail: email }
-        });
-        if (data) {
-            res.status(200).json({ error: `Users ${lastName} already exist` });
-            return;
-        }
+		//CHECK IF EMAIL IS UNIQUE
+		const data = await users.findOne({
+			where: { mail: email },
+		});
+		if (data) {
+			res.status(200).json({ error: `Users ${lastName} already exist` });
+			return;
+		}
 
-        //ENCRYPTER LE PASSWORD DU USER
-        const hash = await bcrypt.hash(password, 10);
-        //CREATE USERS
-        const response = await users.create({
-            first_name: firstName,
-            last_name: lastName,
-            mail: email,
-            password: hash
-        });
-        res.status(200).json({
-            user: response.dataValues,
-            status: "client",
-            token: jwt.sign(
-                { user: response.dataValues.mail },
-                "SHORT_HASH_PHRASE",
-                {
-                    expiresIn: "24h"
-                }
-            )
-        });
-    } catch (error) {
-        res.status(500).json({ error });
-    }
-    return;
+		//ENCRYPTER LE PASSWORD DU USER
+		const hash = await bcrypt.hash(password, 10);
+		//CREATE USERS
+		const response = await users.create({
+			first_name: firstName,
+			last_name: lastName,
+			mail: email,
+			password: hash,
+		});
+		res.status(200).json({
+			user: response.dataValues,
+			status: "client",
+			token: jwt.sign(
+				{ user: response.dataValues.mail },
+				"SHORT_HASH_PHRASE",
+				{
+					expiresIn: "24h",
+				}
+			),
+		});
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+	return;
 };
 
 //AUTHENTIFICATION VIA GOOGLE
@@ -116,44 +116,44 @@ const addUser = async (req, res) => {
  * @returns {Object} JSON object
  */
 const addGoogleUser = async (req, res, next) => {
-    const { userName, userMail } = req.body;
-    //check if user is in database
-    const data = await users.findOne({
-        where: { mail: userMail }
-    });
-    const adminData = await admins.findOne({ where: { email: userMail } });
-    if (adminData && data) {
-        res.status(200).json({ user: data.dataValues, status: "admin" });
-        return;
-    }
-    if (data) {
-        //return user in database
-        res.status(200).json({ user: data.dataValues, status: "client" });
-        return;
-    }
+	const { userName, userMail } = req.body;
+	//check if user is in database
+	const data = await users.findOne({
+		where: { mail: userMail },
+	});
+	const adminData = await admins.findOne({ where: { email: userMail } });
+	if (adminData && data) {
+		res.status(200).json({ user: data.dataValues, status: "admin" });
+		return;
+	}
+	if (data) {
+		//return user in database
+		res.status(200).json({ user: data.dataValues, status: "client" });
+		return;
+	}
 
-    const response = await users.create({
-        first_name: userName,
-        mail: userMail
-    });
+	const response = await users.create({
+		first_name: userName,
+		mail: userMail,
+	});
 
-    res.status(200).json({ user: response.dataValues, status: "client" });
+	res.status(200).json({ user: response.dataValues, status: "client" });
 };
 
 const updateUser = async (req, res) => {
-    return;
+	return;
 };
 
 const deleteUser = async (req, res) => {
-    return;
+	return;
 };
 
 module.exports = {
-    getAllUsers,
-    getUserById,
-    getUser,
-    addUser,
-    addGoogleUser,
-    updateUser,
-    deleteUser
+	getAllUsers,
+	getUserById,
+	getUser,
+	addUser,
+	addGoogleUser,
+	updateUser,
+	deleteUser,
 };
