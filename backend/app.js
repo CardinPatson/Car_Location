6d61872f64d40bcdb2ab07a184ebeaba0aa21ff0
CardinPatson <cardin.tiako@gmail.com>
2022-05-26 23:45:11 +0100

    http server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
 const app = require("./index");
+const fs = require("fs");
+const https = require("https");
 
-app.listen(process.env.APP_PORT, async () => {
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(process.env.APP_PORT, async () => {
 	console.log("server running on port " + process.env.APP_PORT);
 });
 
